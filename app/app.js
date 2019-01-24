b0fe57752f468a7a0e442baafa7d28347958ab1a
Jeremy Dana <jeremy.dana95@gmail.com>
2019-01-24 13:00:28 +0100

    enable cross-origin resource sharing

--- app/app.js
+++ app/app.js
@@ -5,6 +5,12 @@ const io = require("socket.io")(http);
 const port = process.env.PORT || 8181;
 const GcpAPI = require("./services/gcp-api.js");
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 http.listen(port, function() {});
 
 const gcpAPI = new GcpAPI();
